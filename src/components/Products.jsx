3c4e0fb5814d1b2bd1595e40868e9224f2ad6d29
Filip <filfer05@gmail.com>
2022-10-12 01:16:56 +0200

    fix

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -16,7 +16,7 @@ export default function Products({ user, setUser }) {
             }).then(res => res.json())
             .then(data => setProducts(data))
         }
-    }, [])
+    }, [user])
 
     const handleLogout = () => {
         localStorage.removeItem('login')
@@ -25,7 +25,7 @@ export default function Products({ user, setUser }) {
 
     return (
         <section className='flex flex-col items-center relative justify-center h-screen px-[8vw] md:px-[12vw] 2xl:px-[18vw]'>
-            <button className="absolute right-8 top-8" onClick={handleLogout}>Log out</button>
+            <button className="absolute right-8 top-8 font-medium" onClick={handleLogout}>Log out</button>
             <div className='p-12 gap-6  w-full shadow-primaryShadow rounded-3xl'>
                 <h1 className="font-semibold text-3xl">Hi <span className="text-primary">{user.username}</span></h1>
                 {products.length > 0 ? products.map(product => <Product {...product} key={product.id} />) : <h2 className="mt-6 text-[#4A454F]">Nothing's here! Buy something from our <a className="text-primary" href='https://services.divideproject.works/products' target='_blank'>store.</a></h2>}
